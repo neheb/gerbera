0c2586ed9e7f25dd105e44733d1c8781e7f09a01
Rosen Penev <rosenp@gmail.com>
2022-06-02 16:09:45 -0700

    Revert "Handle files from playlists correctly"
    This reverts commit e89a0077bb7b265ddc495832c3931c9a836df203.

--- src/content/content_manager.cc
+++ src/content/content_manager.cc
@@ -448,7 +448,7 @@ std::shared_ptr<CdsObject> ContentManager::createSingleItem(const fs::directory_
 #ifdef HAVE_JS
             try {
                 if (playlist_parser_script && contentType == CONTENT_TYPE_PLAYLIST)
-                    playlist_parser_script->processPlaylistObject(obj, task, rootPath);
+                    playlist_parser_script->processPlaylistObject(obj, task);
             } catch (const std::runtime_error& e) {
                 log_error("{}", e.what());
             }
