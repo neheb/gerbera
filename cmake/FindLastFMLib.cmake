079defa6fbc3842b9d47f26f5e190d9ab39bda0f
Rosen Penev <rosenp@gmail.com>
2022-06-07 19:39:44 -0700

    mangix: remove cmake
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- cmake/FindLastFMLib.cmake
+++
@@ -1,8 +0,0 @@
-INCLUDE (FindPackageHandleStandardArgs)
-
-find_package( PkgConfig REQUIRED )
-PKG_SEARCH_MODULE( LASTFMLIB REQUIRED liblastfm liblastfmlib QUIET)
-
-find_package_handle_standard_args(LASTFMLIB DEFAULT_MSG LASTFMLIB_FOUND)
-
-MARK_AS_ADVANCED( LASTFMLIB_LIBRARIES LASTFMLIB_INCLUDE_DIRS )
